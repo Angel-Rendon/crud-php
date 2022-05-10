faf7e150feb190a2e917700165eeffca836cb0d1
Angel-Rendon <angel_rendon12@hotmail.es>
2022-05-10 15:54:08 -0500

    actualización index.php

--- index.php
+++ index.php
@@ -37,6 +37,10 @@
                 </form>
             </div>
         </div>
+    </div>
+    <!-- scripts bootstrap -->
+    <script src="vendor/components/jquery/jquery.min.js"></script>
+    <script src="vendor/twbs/bootstrap/dist/js/bootstrap.min.js"></script>
 </body>
 
 </html>
