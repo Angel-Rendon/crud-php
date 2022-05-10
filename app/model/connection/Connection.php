7f152cd75cbe58be6a35fa7d480d5b0777bb0d11
Angel-Rendon <angel_rendon12@hotmail.es>
2022-05-10 14:49:29 -0500

    conexión a la db

---
+++ app/model/connection/Connection.php
@@ -0,0 +1,31 @@
+<?php
+
+class Conexion extends PDO
+{
+
+    private $tipo_de_base = 'mysql';
+    private $host = 'localhost';
+    private $nombre_de_base = 'crud_vedurera';
+    private $usuario = 'root';
+    private $contrasena = 'Arendon991215';
+
+    public function __construct()
+    {
+        //Sobreescribo el método constructor de la clase PDO.
+        try {
+            parent::__construct(
+                "{$this->tipo_de_base}:host={$this->host};
+                dbname={$this->nombre_de_base};
+                charset=utf8",
+                $this->usuario,
+                $this->contrasena
+            );
+            echo "conexion exitosa <br>";
+        } catch (PDOException $e) {
+            echo 'Ha surgido un error y no se puede conectar a la base de datos. Detalle: ' . $e->getMessage();
+            exit;
+        }
+    }
+
+}
+$dbConexion = new Conexion();
